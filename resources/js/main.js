0d04f362df1a637a818c028bb9e8ed85504e41be
YourName <noor11809027@gmail.com>
2022-10-15 16:14:41 +0600

    respomsive

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+    // Sticky Menu
+    $(".js--services-section").waypoint(function(direction){
+        if(direction=="down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    })
+
+    // MIX IT UP ( portfolio-section)
     var mixer=mixitup('.portfolio-details');
 })
