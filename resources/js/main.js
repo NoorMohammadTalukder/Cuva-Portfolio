c2171c8c7849f78dad7ec91af3da3e2e1502579b
YourName <noor11809027@gmail.com>
2022-10-15 17:50:49 +0600

    hamburger menu

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,3 +11,11 @@ $(document).ready(function(){
     // MIX IT UP ( portfolio-section)
     var mixer=mixitup('.portfolio-details');
 })
+
+function openNav(){
+    document.getElementById("myNav").style.width="100%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width="0%";
+}
